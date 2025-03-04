38065772ac3d0c60e402dbcb021a6ccb32339a3d
Milan <kaprinyakmilan@gmail.com>
2025-03-04 09:03:44 +0100

    app.js space

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 app.use(cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'], 
     credentials: true
 }));
 app.use(cookieParser());
