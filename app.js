b4d27615c1fee593a9519dabd8f43cf643b7477a
levente04 <vincze.levente.37@dszcbaross.edu.hu>
2025-03-06 08:45:15 +0100

    tablet es laptopok megjelenitese

--- app.js
+++ app.js
@@ -117,6 +117,32 @@ app.get("/api/getPhones", (req, res) => {
     });
 });
 
+app.get("/api/getTablets", (req, res) => {
+    const sql = "SELECT * FROM products WHERE itemCategory = 'Tablet'";
+
+    pool.query(sql, (error, results) => {
+        if (error) {
+            console.error("Error fetching products:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+            return;
+        }
+        res.json(results);
+    });
+});
+
+app.get("/api/getLaptops", (req, res) => {
+    const sql = "SELECT * FROM products WHERE itemCategory = 'Laptop'";
+
+    pool.query(sql, (error, results) => {
+        if (error) {
+            console.error("Error fetching products:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+            return;
+        }
+        res.json(results);
+    });
+});
+
 app.get('/api/search/:searchQuery', authenticateToken, (req, res) => {
     const searchQuery = req.params.searchQuery;
     console.log(searchQuery);
