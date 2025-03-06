ca8acc03439bcc8bef11902ce8db7fba09e2e04f
levente04 <vincze.levente.37@dszcbaross.edu.hu>
2025-03-06 08:38:38 +0100

    *

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.get("/api/getProducts", (req, res) => {
 app.get("/api/getPhones", (req, res) => {
     const sql = "SELECT * FROM products WHERE itemCategory = 'Mobiltelefon'";
 
-    connection.query(sql, (error, results) => {
+    pool.query(sql, (error, results) => {
         if (error) {
             console.error("Error fetching products:", error);
             res.status(500).json({ error: "Internal Server Error" });
